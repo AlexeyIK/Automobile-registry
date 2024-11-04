e21d693073c2bd715896bb39eb066563071d8598
akuznetsov <akuznetsov@vizex.ru>
2024-11-05 00:32:13 +0300

    вход с проверкой логина и пароля

--- LoginScreen.xaml.cs
+++ LoginScreen.xaml.cs
@@ -2,14 +2,44 @@
 
 public partial class LoginScreen : ContentPage
 {
-	public LoginScreen()
-	{
-		InitializeComponent();
-	}
+    private const string EMPTY_FIELD_ALERT = "Необходимо заполнить поля \"Логин\" и \"Пароль\"!";
+    private const string WRONG_LOGINPASSORD_ALERT = "Неверные логин/пароль";
 
-	private async void OnSubmitBtnClicked(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new MainMenu(), true);
-	}
+    private string mLogin = "admin";
+    private string mPassword = "1234";
+
+    public LoginScreen()
+    {
+        InitializeComponent();
+    }
+
+    private void LoginPasswordTextChanged(object sender, EventArgs e)
+    {
+        if (AlertLabel.IsVisible)
+            AlertLabel.IsVisible = false;
+    }
+
+    private async void OnSubmitBtnClicked(object sender, EventArgs e)
+    {
+        if (String.IsNullOrWhiteSpace(LoginEntry.Text) || String.IsNullOrWhiteSpace(PasswordEntry.Text))
+        {
+            AlertLabel.IsVisible = true;
+            AlertLabel.Text = EMPTY_FIELD_ALERT;
+            return;
+        }
+
+        if (LoginEntry.Text != mLogin || PasswordEntry.Text != mPassword)
+        {
+            AlertLabel.IsVisible = true;
+            AlertLabel.Text = WRONG_LOGINPASSORD_ALERT;
+            return;
+        }
+
+        await Navigation.PushAsync(new MainMenu(), true);
+    }
+
+    void PasswordEntry_TextChanged(System.Object sender, Microsoft.Maui.Controls.TextChangedEventArgs e)
+    {
+    }
 }
 
