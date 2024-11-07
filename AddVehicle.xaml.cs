1749dab252ceccdd559f79f3128c5a22e9d9edbb
akuznetsov <akuznetsov@vizex.ru>
2024-11-07 20:40:43 +0300

    немного стилей, но кривых

--- AddVehicle.xaml.cs
+++ AddVehicle.xaml.cs
@@ -12,7 +12,6 @@ public partial class AddVehicle : ContentPage
 	{
 		using (var db = new ApplicationContext())
 		{
-			// Загрузка данных для выпадающих списков
 			ManufacturerPicker.ItemsSource = db.Manufacturers.Select(m => m.Name).ToList();
 			TypePicker.ItemsSource = db.VehicleTypes.Select(t => t.Name).ToList();
 			ColorPicker.ItemsSource = db.Colors.Select(c => c.ColorName).ToList();
@@ -21,11 +20,21 @@ public partial class AddVehicle : ContentPage
 
 	private async void OnCreateBtnClicked(object sender, EventArgs e)
 	{
-		// Добавьте валидацию и сохранение данных
+		// ToDo: добавлять запись в БД
 	}
 
 	private async void OnCancelBtnClicked(object sender, EventArgs e)
 	{
 		await Navigation.PopAsync();
+	}
+
+	private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+	{
+		var picker = (Picker)sender;
+		if (picker.SelectedIndex != -1)
+		{
+			var parent = (Border)picker.Parent.Parent;
+			parent.Stroke = Colors.DarkCyan;
+		}
 	}
 }
